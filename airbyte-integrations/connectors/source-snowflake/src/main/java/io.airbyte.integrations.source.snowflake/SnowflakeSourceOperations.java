59d53447e7ae2a678246dff4c2fde6e52c764b9b
Samuel Henrique Ferreira <samuel.ferreira@bitboundaire.com>
2024-06-21 09:40:47 -0300

    hard coding array maping

--- airbyte-integrations/connectors/source-snowflake/src/main/java/io.airbyte.integrations.source.snowflake/SnowflakeSourceOperations.java
+++ airbyte-integrations/connectors/source-snowflake/src/main/java/io.airbyte.integrations.source.snowflake/SnowflakeSourceOperations.java
@@ -111,11 +111,14 @@ public class SnowflakeSourceOperations extends JdbcSourceOperations {
   public void copyToJsonField(final ResultSet resultSet, final int colIndex, final ObjectNode json) throws SQLException {
     final String columnName = resultSet.getMetaData().getColumnName(colIndex);
     final String columnTypeName = resultSet.getMetaData().getColumnTypeName(colIndex).toLowerCase();
-    LOGGER.info(String.format("Received copyToJsonField columnName: %s and typeName %s ", columnName, columnTypeName));
+    LOGGER.info(String.format("Received copyToJsonField columnName: %s and typeName %s and colTypeIndex", columnName, columnTypeName, resultSet.getMetaData().getColumnType(colIndex)));
     // TIMESTAMPLTZ data type detected as JDBCType.TIMESTAMP which is not correct
     if ("TIMESTAMPLTZ".equalsIgnoreCase(columnTypeName)) {
       putTimestampWithTimezone(json, columnName, resultSet, colIndex);
       return;
+    }
+    if ("ARRAY".equalsIgnoreCase(columnTypeName)) {
+      putArray(json, columnName, resultSet, colIndex);
     }
     super.copyToJsonField(resultSet, colIndex, json);
   }
